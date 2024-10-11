9017128fd41888d4595272b9ebd0311e527b36fc
Hydropper <66328857+Hydropper@users.noreply.github.com>
2024-10-10 22:09:54 -0400

    Copy spriteFrame added
    Haven't tested this on other platforms besides windows; other OS users please let me know if there are any issues

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aseprite-RSDK
+
+
+# How to use
+Install: Open the .aseprite-extension file from the releases tab corresponding to your OS, OR drag and drop the corresponding folder from source into your extensions folder.
+Usage: In aseprite, go to Edit->Preferences->Extensions, and make sure RSDK Scripts is enabled. From there, all tools should be accessible in their corresponding menus.
+
+
 Tools for aseprite to make RSDK modding a little bit easier
+Current tools: 
+- SpriteFrame generator [Edit->Copy SpriteFrame] or Ctrl + Alt + C
+
+Forks are allowed, but please do not distribute without credit
---
+++ Windows/RSDK Scripts/rsdk-scripts.lua
@@ -0,0 +1,70 @@
+function copySpriteframe()
+  local sprite = app.activeSprite
+  
+  if sprite == nil then
+    app.alert("No active sprite")
+    return
+  end
+  
+  -- Get the selection bounds
+  local selectionBounds = sprite.selection.bounds
+  
+  if selectionBounds == nil or selectionBounds.isEmpty then
+    app.alert("No selection")
+    return
+  end
+  
+  -- Get width, height, x position, y position
+  local width = selectionBounds.width
+  local height = selectionBounds.height
+  local xpos = selectionBounds.x
+  local ypos = selectionBounds.y
+  
+  -- Calculate the center of the selection and round down
+  local centerX = math.floor(width / 2)
+  local centerY = math.floor(height / 2)
+  
+  -- Format as (-centerX, -centerY, width, height, xpos, ypos)
+  local formattedText = string.format("SpriteFrame(-%d, -%d, %d, %d, %d, %d)", centerX, centerY, width, height, xpos, ypos)
+  
+  -- Copy to clipboard
+  -- Windows
+  local command = 'echo ' .. formattedText .. ' | clip'
+  
+  -- MacOS
+  -- local command = 'echo "' .. formattedText .. '" | pbcopy'
+  
+  -- Linux (xclip only)
+  -- local command = 'echo "' .. formattedText .. '" | xclip -selection clipboard'
+  
+  os.execute(command)
+  
+  -- app.alert("SpriteFrame copied to clipboard: " .. formattedText) // If you want an alert for some reason
+end
+
+function init(plugin)
+    local parentGroup = "edit_transform"
+
+    if app.apiVersion >= 22 then
+
+        plugin:newMenuGroup{
+            id = parentGroup,
+            title = "RSDK",
+            group = "edit_transform"
+        }
+    end
+
+    plugin:newCommand{
+        id = "SpriteFrame",
+        title = "Copy SpriteFrame",
+        group = parentGroup,
+        onclick = function()
+           copySpriteFrame()
+        end
+    }
+end
+
+function exit(plugin)
+  print("Aseprite is closing my plugin, MyFirstCommand was called "
+        .. plugin.preferences.count .. " times")
+end
